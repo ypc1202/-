2564014693bc70464c9935ed0a00fe57415316a4
mtkowen <mtkowen@hotmail.com>
2016-10-23 17:26:04 +0800

    delete some folders

--- Product/Demo/Code/demo.h
+++
@@ -1,9 +0,0 @@
-/******************************************************************************/
-/*                                                                            */
-/*   DEMO.H:  Project specific header for the Demo example                    */
-/*                                                                            */
-/******************************************************************************/
-
-#define STARTUP_TASK      0                   /* task number of task:  startup_task   */
-#define PHASE1            1                   /* task number of task:  phase1         */
-#define PHASE2            2                   /* task number of task:  phase2         */
